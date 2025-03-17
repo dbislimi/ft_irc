7b3b1af9365442377de3a26aa64760e720cc0ce9
Bilal Safi <bsafi@c1r2p8.42nice.fr>
2025-03-17 11:27:46 +0100

    verify username

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: bsafi <bsafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 14:55:52 by dbislimi          #+#    #+#             */
-/*   Updated: 2025/03/13 16:29:35 by bsafi            ###   ########.fr       */
+/*   Updated: 2025/03/17 11:26:34 by bsafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 bool Server::signal = false;
 
-void Server::signals(int signum){
+void Server::signals(int signum)
+{
 	(void)signum;
 	std::cout << "signal received " << std::endl;
 	Server::signal = true;
 }
 
-void	Server::_init_socket(){ 
-	struct	sockaddr_in	sa;
-	struct	pollfd		poll;
+void Server::_init_socket()
+{
+	struct sockaddr_in sa;
+	struct pollfd poll;
 
 	sa.sin_family = AF_INET;
 	sa.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
@@ -32,13 +34,13 @@ void	Server::_init_socket(){
 	if (this->_serverFd == -1)
 		throw std::runtime_error("Error: socket init failed.");
 	std::cout << "Socket created: " << this->_serverFd << std::endl;
-	
-	int	en = 1;
+
+	int en = 1;
 	if (setsockopt(this->_serverFd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)))
 		throw std::runtime_error("Error: failed to set option SO_REUSEADDR on socket.");
 	if (fcntl(_serverFd, F_SETFL, O_NONBLOCK))
 		throw std::runtime_error("Error: failed to set option O_NONBLOCK on socket.");
-	if (bind(this->_serverFd, reinterpret_cast<struct sockaddr*>(&sa), sizeof(sa)))
+	if (bind(this->_serverFd, reinterpret_cast<struct sockaddr *>(&sa), sizeof(sa)))
 		throw std::runtime_error("Error: bind failed.");
 	if (listen(this->_serverFd, BACKLOG))
 		throw std::runtime_error("Error: failed to listen.");
@@ -48,31 +50,36 @@ void	Server::_init_socket(){
 	_fds.push_back(poll);
 }
 
-void	Server::init(){
+void Server::init()
+{
 	_init_socket();
-	while (Server::signal == false){
-		if ((poll(&_fds[0], _fds.size(), -1) == -1) && Server::signal == false){
+	while (Server::signal == false)
+	{
+		if ((poll(&_fds[0], _fds.size(), -1) == -1) && Server::signal == false)
+		{
 			throw std::runtime_error("Error: function poll failed");
 		}
-		for (size_t i = 0; i < _fds.size(); ++i){
-			if (_fds[i].revents & POLLIN){
+		for (size_t i = 0; i < _fds.size(); ++i)
+		{
+			if (_fds[i].revents & POLLIN)
+			{
 				if (_fds[i].fd == _serverFd)
 					newClient();
 				else
 					newCmd(_fds[i].fd);
-					
 			}
 		}
 	}
 }
 
-void	Server::newClient(){
+void Server::newClient()
+{
 	struct sockaddr_in sa;
 	int clientfd;
 	socklen_t size = sizeof(sa);
 	struct pollfd paul;
 	Client *clicli = new Client();
-	clientfd = accept(this->_serverFd, reinterpret_cast<struct sockaddr*>(&sa), &size);
+	clientfd = accept(this->_serverFd, reinterpret_cast<struct sockaddr *>(&sa), &size);
 	if (clientfd == -1)
 		throw std::runtime_error("Error: function accept failed");
 	if (fcntl(clientfd, F_SETFL, O_NONBLOCK))
@@ -82,30 +89,33 @@ void	Server::newClient(){
 	paul.revents = 0;
 	clicli->setFd(clientfd);
 	clicli->setSign(false);
+	clicli->setBoolName(false);
 	std::cout << "IP = " << clicli->getFd() << std::endl;
 	clicli->setIpAdd(sa.sin_addr);
-	_clients.insert(std::pair<int, Client*>(clientfd, clicli));
+	_clients.insert(std::pair<int, Client *>(clientfd, clicli));
 	_fds.push_back(paul);
-	intro(clientfd);
 	std::cout << "Client <" << clientfd << "> Connected" << std::endl;
 	printmap();
 }
 
-
-void	Server::newCmd(int fd){
-	char	buff[1024];
+void Server::newCmd(int fd)
+{
+	char buff[1024];
 	memset(buff, 0, sizeof(buff));
-	size_t	bytes = recv(fd, buff, sizeof(buff) - 1, 0);
-	if (bytes <= 0){
+	size_t bytes = recv(fd, buff, sizeof(buff) - 1, 0);
+	if (bytes <= 0)
+	{
 		std::cout << "Client " << fd << " disconnected." << std::endl;
 		eraseClient(fd);
 		close(fd);
-		return ;
+		return;
 	}
 	buff[bytes] = 0;
 	_cmd = parseCmd(buff);
-	for(std::map<int, Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it){
-		if (it->second->getSign() == false){
+	for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+	{
+		if (it->second->getSign() == false)
+		{
 			checkPassword(fd);
 			if (it->second->getSign() == true)
 				intro(fd);
@@ -113,41 +123,51 @@ void	Server::newCmd(int fd){
 		else
 			handleCmd(_cmd, fd);
 	}
-	std::cout << "Client <" << fd << "> Data: "<< buff << std::endl;
+	std::cout << "Client <" << fd << "> Data: " << buff << std::endl;
 }
 
-void Server::checkPassword(int fd){
+void Server::checkPassword(int fd)
+{
 	trim(_passWord);
 	std::string pass;
-	if (_cmd[0] == _passWord){
+	if (_cmd[0] == _passWord)
+	{
 		pass = "valid passeword\r\n";
 		send(fd, pass.c_str(), pass.length(), 0);
-		for(std::map<int, Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it){
-			if (it->first == fd){
+		for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+		{
+			if (it->first == fd)
+			{
 				it->second->setSign(true);
 			}
 		}
 	}
-	else{
+	else
+	{
 		pass = "wrong password, retry \r\n";
-    	send(fd, pass.c_str(), pass.length(), 0);
+		send(fd, pass.c_str(), pass.length(), 0);
 	}
 }
 
-Server::~Server(){
-	for (size_t i = 0; i < _fds.size(); ++i){
+Server::~Server()
+{
+	for (size_t i = 0; i < _fds.size(); ++i)
+	{
 		std::cout << "closing fd: " << _fds[i].fd << std::endl;
 		close(_fds[i].fd);
 	}
-	for (std::map<int, Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+	for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
 		delete it->second;
 	_clients.clear();
 	_fds.clear();
 }
 
-void	Server::printmap(){
-	std::cout << std::endl << "Map: " << std::endl;
-	for (std::map<int, Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it){
+void Server::printmap()
+{
+	std::cout << std::endl
+			  << "Map: " << std::endl;
+	for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+	{
 		char buffer[INET6_ADDRSTRLEN];
 		inet_ntop(AF_INET, &((it->second)->getIp()), buffer, sizeof buffer);
 		std::cout << "[" << it->first << "] = " << buffer << std::endl;
@@ -155,55 +175,82 @@ void	Server::printmap(){
 	std::cout << std::endl;
 }
 
-void	Server::eraseClient(int fd){
-	for(std::vector<struct pollfd>::iterator it = _fds.begin(); it != _fds.end(); ++it){
-		if ((*it).fd == fd){
-			_fds.erase(it); 
+void Server::eraseClient(int fd)
+{
+	for (std::vector<struct pollfd>::iterator it = _fds.begin(); it != _fds.end(); ++it)
+	{
+		if ((*it).fd == fd)
+		{
+			_fds.erase(it);
 			break;
 		}
 	}
-	std::map<int, Client*>::iterator it = _clients.find(fd);
+	std::map<int, Client *>::iterator it = _clients.find(fd);
 	if (it == _clients.end())
-		return ;
+		return;
 	delete it->second;
 	_clients.erase(it);
 }
 
-void	Server::handleCmd(std::deque<std::string> cmd, int fd){
-	std::map<int, Client*>::iterator it = _clients.find(fd);
-	if (it->second->getBoolName() == false){
+void Server::handleCmd(std::deque<std::string> cmd, int fd)
+{
+	std::map<int, Client *>::iterator it = _clients.find(fd);
+	std::cout << fd << "  Boolname: " << it->second->getBoolName() << std::endl;
+	if (it->second->getBoolName() == false)
+	{
 		std::string ask = "entry nickname:\r\n";
 		send(fd, ask.c_str(), ask.length(), 0);
-		it->second->setNickName(_cmd[0]);
-		it->second->setBoolName(true);
+		for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+		{
+			if (_cmd[0] == it->second->getNickName())
+			{
+				it->second->setBoolName(false);
+				ask = "Nickname already taken\r\t";
+				send(fd, ask.c_str(), ask.length(), 0);
+				break;
+			}
+			else
+				it->second->setBoolName(true);
+		}
+		if (it->second->getBoolName() == true)
+			it->second->setNickName(_cmd[0]);
 	}
 	std::string cmds[7] = {"NICK", "USER", "JOIN", "KICK", "INVITE", "TOPIC", "MODE"};
-	void	(Server::*funct[7])() = {&Server::NICK, &Server::USER, &Server::JOIN, &Server::KICK, &Server::INVITE, &Server::TOPIC, &Server::MODE};
-	
-	for (int i = 0; i < 7; ++i){
+	void (Server::*funct[7])() = {&Server::NICK, &Server::USER, &Server::JOIN, &Server::KICK, &Server::INVITE, &Server::TOPIC, &Server::MODE};
+
+	for (int i = 0; i < 7; ++i)
+	{
 		if (cmd[0] == cmds[i])
 			(this->*funct[i])();
 	}
 }
 
-void	Server::NICK(){
+void Server::NICK()
+{
 }
-void	Server::USER(){
+void Server::USER()
+{
 }
-void	Server::JOIN(){
+void Server::JOIN()
+{
 }
-void	Server::KICK(){
+void Server::KICK()
+{
 }
-void	Server::INVITE(){
+void Server::INVITE()
+{
 }
-void	Server::TOPIC(){
+void Server::TOPIC()
+{
 }
-void	Server::MODE(){
+void Server::MODE()
+{
 }
 
-void	Server::intro(int clientfd){
+void Server::intro(int clientfd)
+{
 	ssize_t bytes;
-	
+
 	std::string test = "welcome to the TEST Internet Relay Chat Network\r\n";
 	bytes = send(clientfd, test.c_str(), test.size(), 0);
 	if (bytes == -1)
@@ -218,8 +265,9 @@ void	Server::intro(int clientfd){
 		throw std::runtime_error("Error: function send failed");
 	std::string ask = "entry username:\r\n";
 	send(clientfd, ask.c_str(), ask.length(), 0);
-	std::map<int, Client*>::iterator it = _clients.find(clientfd);
-	if (it == _clients.end()){
+	std::map<int, Client *>::iterator it = _clients.find(clientfd);
+	if (it == _clients.end())
+	{
 		throw std::runtime_error("Error : not find client");
 	}
 	it->second->setUserName(_cmd[0]);
