48fef9ffbd9fa1ea2c5fa9f01f5cd6d936600f47
Dylan Ravaonoromanana <dravaono@c1r4p3.42nice.fr>
2025-04-09 18:23:12 +0200

    added function addOp in class Channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: dravaono <dravaono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:40:16 by dbislimi          #+#    #+#             */
-/*   Updated: 2025/04/09 18:08:06 by dravaono         ###   ########.fr       */
+/*   Updated: 2025/04/09 18:22:44 by dravaono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,12 @@
 class Channel {
 	private:
 		std::string			_name;
-		// std::map<std::string, int>	_users;
-		std::deque<int>					_ops;
+		std::deque<int>		_ops;
 
 	public:
-		Channel(int op, const std::string& name): _name(name){_ops.insert(_ops.end(), op);}
+		Channel(int op, const std::string& name): _name(name){_ops.push_back(op);}
 
-		// void	add(int fd, std::string nick);
-		// void	sendChannel(int fd, std::string msg);
-		// void	joinChannel(std::string channel);
-		// bool	findChannel(std::string value);
 		bool		isOp(int fd);
-		// bool	findUser(std::string nick);
 		void	removeOp(int fd);
-		// void	erasefd(int fd);
-		// int		findFd(std::string nick);
-		// std::string findNick(int fd);
-		// std::map<std::string, int>	getUsers();
+		void	addOp(int fd);
 };
--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -1,37 +1,11 @@
 #include "../includes/all.hpp"
 
-
-// void Channel::delete_user(int fd) {
-//     for (size_t i = 0; i < _users.size(); ++i) {
-//         if (_users[i] == fd) {
-//             std::swap(_users[i], _users.back());
-//             _users.pop_back();
-//             break;
-//         }
-//     }
-// }
-
-// void	Channel::sendChannel(int fd, std::string msg){
-// 	for (std::map<std::string, int>::iterator it = _users.begin(); it != _users.end(); ++it){
-//         if (fd == -1 || it->second != fd)
-// 			send(it->second, msg.c_str(), msg.length(), 0);
-//     }
-// }
-
 bool	Channel::isOp(int fd){
 	for (std::deque<int>::iterator it = _ops.begin(); it != _ops.end(); ++it)
 		if (*it == fd)
 			return (true);
 	return (false);
 }
-
-// bool	Channel::findUser(std::string nick) {
-// 	for (std::map<std::string, int>::iterator it = _users.begin(); it != _users.end(); ++it){
-// 		if (it->first == nick)
-// 			return (true);
-// 	}
-// 	return (false);
-// }
 
 void	Channel::removeOp(int fd){
 	for (std::deque<int>::iterator it = _ops.begin(); it != _ops.end(); ++it){
@@ -42,3 +16,6 @@ void	Channel::removeOp(int fd){
 	}
 }
 
+void    Channel::addOp(int fd){
+    _ops.push_back(fd);
+}
