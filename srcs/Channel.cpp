74c5c36cfafad515fa557bcca82d8b466d88b38b
dren <dren.bislimiapo@gmail.com>
2025-04-29 15:51:37 +0200

    norm invite

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -120,106 +120,6 @@ std::string	Channel::getModes(){
 	return (str);
 }
 
-// bool	Channel::getInvitRestrict(){
-// 	return(_invitRestrict);
-// }
-
-// void	Channel::setInvitRestrict(bool value){
-// 	_invitRestrict = value;
-// }
-
-// std::string	Channel::getMdp(){
-// 	return (_mdp);
-// }
-
-// void	Channel::setMdp(std::string w){
-// 	_mdp = w;
-// }
-
-// bool		Channel::getIsmdp(){
-// 	return _ismdp;
-// }
-
-// void		Channel::setIsmdp(bool val){
-// 	_ismdp = val;
-// }
-
-// size_t			Channel::getLimitUser(){
-// 	return _limitUser;
-// }
-
-// void		Channel::setLimitUser(size_t val){
-// 	_limitUser = val;
-// }
-
-// void		Channel::setTopicRestrict(bool val){
-// 	_isTopicRestrict = val;
-// }
-
-// bool		Channel::getTopicRestrict(){
-// 	return (_isTopicRestrict);
-// }
-
-// void	Channel::setTime(){
-// 	_timestamp = std::time(NULL);
-// }
-
-// std::time_t	Channel::getTime() const{
-// 	return (_timestamp);
-// }
-
-// void		Channel::setTopic(std::string topic){
-// 	_topic = topic;
-// }
-
-// std::string	Channel::getTopic() const{
-// 	return (_topic);
-// }
-
-// bool		Channel::getisLimitUser(){
-// 	return _isLimitUser;
-// }
-
-// void		Channel::setisLimitUser(bool val){
-// 	_isLimitUser = val;
-// }
-
-// void		Channel::updateModes(std::deque<std::string> cmd){
-// 	char mode = cmd[2][1];
-
-// 	if (cmd[2][0] == '+')
-// 		_modes.push_back(mode);
-// 	else{
-// 		size_t pos = _modes.find(mode);
-// 		if (pos != std::string::npos)
-// 			_modes.erase(pos, 1);
-// 	}
-// }
-
-// std::string	Channel::getModes(){
-// 	std::string str = "";
-// 	bool		k = 0;
-// 	bool		l = 0;
-
-// 	for (size_t i = 0; i < _modes.size(); ++i){
-// 		if (_modes[i] == 'k')
-// 			k = 1;
-// 		else if (_modes[i] == 'l')
-// 			l = 1;
-// 		else
-// 			str += _modes[i];
-// 	}
-// 	if (l)
-// 		str += "l";
-// 	if (k)
-// 		str += 'k';
-// 	if (l)
-// 		str += " " + longToString(_limitUser);
-// 	if (k)
-// 		str += " " + _mdp;
-// 	return (str);
-// }
-
 void Channel::setChannelOnInvited(bool channelOnInvited){
     this->_channelOnInvited = channelOnInvited;
 }
@@ -228,13 +128,6 @@ bool Channel::getChannelOnInvited(){
     return this->_channelOnInvited;
 }
 
-// void Channel::setChannelOnInvited(bool channelOnInvited){
-//     this->_channelOnInvited = channelOnInvited;
-// }
-
-// bool Channel::getChannelOnInvited(){
-//     return this->_channelOnInvited;
-// }
 std::string Channel::getName(){
 	return _name;
 }
--- srcs/invite.cpp
+++ srcs/invite.cpp
@@ -6,23 +6,23 @@ void	Server::INVITE(int fd, std::deque<std::string> cmd){
 		return ;
 	}
 	if (!_channels[cmd[2]]->isOp(fd)){
-		mysend(fd, ":serveur 482 " + _clients[fd]->getNickName() + " " + cmd[1] + " :Permission Denied- You're not an IRC operator\r\n");
+		mysend(fd, ":serveur 482 " + _clients[fd]->getNickName() + " " + cmd[1] + " :You're not channel operator\r\n");
 		return ;
 	}
-	if (cmd.size() != 3){
-		mysend(fd, "Usage: INVITE <nick> [<channel>], invites someone to a channel, by default the current channel\r\n");
+	if (cmd.size() < 3){
+		mysend(fd, ":server 461 " + _clients[fd]->getNickName() +  " " + cmd[0] + " :Not enough parameters\r\n");
 		return ;
 	}
 	if (!checkClient(cmd[1])){
-		mysend(fd, cmd[1] + " :No such nick\r\n");
+		mysend(fd, ":serveur 401 " + _clients[fd]->getNickName() + " " + cmd[1] + " :No such nick\r\n");
 		return ;
 	}
-	if (_clients[fd]->getNickName() == cmd[1]){
-		mysend(fd, ":serveur 443 " + _clients[fd]->getNickName() + " " + cmd[2] + " :is already on channel");
+	if (_clients[fd]->getNickName() == cmd[1]){ // condition pas bonne faut check tous les clients dans le channel
+		mysend(fd, ":serveur 443 " + _clients[fd]->getNickName() + " "  + cmd[1] + " " + cmd[2] + " :is already on channel");
 		return ;
 	}
 	if (!checkChannel(cmd[2])){
-		mysend(fd, cmd[2] + " :No such channel\r\n");
+		mysend(fd, ":server 403 " + _clients[fd]->getNickName() +  " " + cmd[2] + " :No such channel\r\n");
 		return ;
 	}
 	std::map<int, Client*>::iterator ot = _clients.begin();
@@ -38,7 +38,8 @@ void	Server::INVITE(int fd, std::deque<std::string> cmd){
 		it++;
 	}
 	it->second->setInvited(cmd[2], true);
-	
-	mysend(it->second->getFd(), "You have been invited to \00306" + cmd[2] + "\003 by \00302" + ot->second->getNickName() + "\003\r\n");
-	mysend(fd, "You've invited \00302" + it->second->getNickName() + "\003 to \00306" + cmd[2] + "\003\r\n");
+	mysend(it->second->getFd(), ":" + _clients[fd]->getNickName() + "!" + _clients[fd]->getUserName() + "@" + _clients[fd]->getIp() + " INVITE " + cmd[1] + " "  + cmd[2] + "\r\n");
+	mysend(fd, ":server 341 " + _clients[fd]->getNickName() +  " " + cmd[1] +  + " " + cmd[2] + "\r\n");
+	// mysend(it->second->getFd(), "You have been invited to \00306" + cmd[2] + "\003 by \00302" + ot->second->getNickName() + "\003\r\n");
+	// mysend(fd, "You've invited \00302" + it->second->getNickName() + "\003 to \00306" + cmd[2] + "\003\r\n");
 }
